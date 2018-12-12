e8afee9b1134279a3cf583281862b588df5c3d8e
Fang You-Jyun <eugenefang565@gmail.com>
2018-12-12 16:12:20 +0800

    登入與登出

--- web/urls.py
+++ web/urls.py
@@ -4,8 +4,8 @@ from .views import *
 
 urlpatterns = [
     path('', RedirectView.as_view(url='message/')),
-    path('message/', MessageListView.as_view()),
-    path('message/<int:pk>/', MessageDetailView.as_view()),
+    path('message/', MessageList.as_view()),
+    path('message/<int:pk>/', MessageDetail.as_view()),
     path('message/create/', MessageCreate.as_view()),
     path('message/<int:pk>/delete/', MessageDelete.as_view()),
 ]
